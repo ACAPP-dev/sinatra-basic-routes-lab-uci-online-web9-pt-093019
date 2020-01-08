55c43260a74311fb56b12a3e436eaeba8193608b
Andrew Capp <acapp909@gmail.com>
2020-01-08 23:24:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is __"
+    "My name is Andrew"
+  end
+
+  get '/hometown' do
+    "My hometown is Durango"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Help"
   end
 end
